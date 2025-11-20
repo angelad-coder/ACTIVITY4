badc781890279ef7f2565b81d2cb3cccf0dd373d
angelad-coder <gelaandal05@gmail.com>
2025-11-20 13:09:43 +0800

    feat/insert-10-rows-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -97,3 +97,15 @@ INSERT INTO doctors VALUES
 (9,'Dr.Ryan','Delos','Nutrition','7778889999','ryan@example.com'),
 (10,'Dr.Grace','Lopez','Surgery','6665554444','grace@example.com');
 
+INSERT INTO invoices(invoiceid, appointid, totalamount, paymentdate)
+VALUES
+(1, 1, 50.00, '09:30:00'),
+(2, 2, 75.00, '14:15:00'),
+(3, 3, 100.00, '11:00:00'),
+(4, 4, 200.00, '13:45:00'),
+(5, 5, 80.00, '10:30:00'),
+(6, 6, 30.00, '15:00:00'),
+(7, 7, 75.00, '09:15:00'),
+(8, 8, 150.00, '16:30:00'),
+(9, 9, 60.00, '14:45:00'),
+(10, 10, 40.00, '11:30:00');
